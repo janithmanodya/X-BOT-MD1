fcc42912a9e46b4aeb841d29ff31a1674cfe53c4
janithmanodya <142404619+janithmanodya@users.noreply.github.com>
2024-04-13 09:35:14 +0530

    Update config.js

--- config.js
+++ config.js
@@ -9,14 +9,14 @@ if (fs.existsSync('config.env')) {
 
 const API = "https://api-aswin-sparky.koyeb.app";
 const HANDLERS = process.env.HANDLER || "^";
-const SESSION_ID = process.env.SESSION_ID || "";
-const SUDO = process.env.SUDO || '917012984396';
-const AUTO_STATUS_VIEW = process.env.AUTO_STATUS_VIEW || "true";
+const SESSION_ID = process.env.SESSION_ID || "X-BOT-MD:ShakesSayonara";
+const SUDO = process.env.SUDO || '94770889232';
+const AUTO_STATUS_VIEW = process.env.AUTO_STATUS_VIEW || "false";
 const ALWAYS_ONLINE = process.env.ALWAYS_ONLINE || "true";
-const BOT_INFO = process.env.BOT_INFO || "X-BOT-MD;ASWIN SPARKY;https://i.imgur.com/r3GZeiX.jpeg";
-const URL = process.env.URL || "https://www.instagram.com/imspxxky";
-const AUDIO_DATA = process.env.AUDIO_DATA || "X BOT MD;ASWIN SPARKY;https://i.imgur.com/fVCRCYG.jpeg";
-const STICKER_DATA = process.env.STICKER_DATA || "X BOT MD;ASWIN SPARKY";
+const BOT_INFO = process.env.BOT_INFO || "JW BOT V2";
+const URL = process.env.URL || "https://www.instagram.com/janithmanodaya";
+const AUDIO_DATA = process.env.AUDIO_DATA || "JW BOT V2";
+const STICKER_DATA = process.env.STICKER_DATA || "JW BOT V2";
 const WORK_TYPE = process.env.WORK_TYPE || 'public';
 const DATABASE_URL = process.env.DATABASE_URL || "./lib/database.db";
 const HEROKU_APP_NAME = process.env.HEROKU_APP_NAME || "";
